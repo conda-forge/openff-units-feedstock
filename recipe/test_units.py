dd5a3d7bf3cac7a85be3507c5d477d1fec6ac1cf
Matthew W. Thompson <mattwthompson@protonmail.com>
2024-03-25 12:36:12 -0500

    Beef up test

--- recipe/test_units.py
+++ recipe/test_units.py
@@ -12,3 +12,10 @@ assert unit.Quantity("1 nanometer") == Quantity("1 nanometer")
 assert MASSES[1].m_as(unit.dalton) == 1.007947
 assert NUMBERS["Hg"] == 80
 assert SYMBOLS[79] == "Au"
+
+print(
+    "Tests appear to have passed!"
+    f"\n{(0.5 * unit.kilojoule_per_mole)=},"
+    f"\n{([4, 4, 4] * unit.nanometer)=},"
+    f"\n{SYMBOLS[79]=}",
+)
